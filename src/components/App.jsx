d14391adc650e32f4dff97ad5ce66b6cc3308d83
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-09 15:39:56 +1100

    final

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./heading.jsx";
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
